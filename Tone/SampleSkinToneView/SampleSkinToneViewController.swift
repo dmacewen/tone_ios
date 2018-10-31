cfab622ea108561f87f78c753b9262145bf20ddd
Doug MacEwen <d2macewen@gmail.com>
2018-10-31 15:18:43 -0700

    Basic setup for capture done

--- Tone/SampleSkinToneView/SampleSkinToneViewController.swift
+++ Tone/SampleSkinToneView/SampleSkinToneViewController.swift
@@ -15,6 +15,13 @@ class SampleSkinToneViewController: UIViewController {
     var viewModel: SampleSkinToneViewModel!
     
     @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var takeSampleButton: UIButton!
+    
+    @IBOutlet weak var rootView: UIView!
+    @IBOutlet weak var bottomFlash: UIView!
+    @IBOutlet weak var topFlash: UIView!
+    
+    @IBOutlet weak var userPrompt: UITextField!
     
     let disposeBag = DisposeBag()
     
@@ -26,5 +33,20 @@ class SampleSkinToneViewController: UIViewController {
             .single()
             .subscribe(onNext: { _ in self.viewModel.cancel() })
             .disposed(by: disposeBag)
+
+        viewModel.userFaceState.asObserver()
+            .map { $0.message }
+            .bind(to: userPrompt.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.userFaceState.asObserver()
+            .map { $0 == .ok }
+            .bind(to: takeSampleButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        takeSampleButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.viewModel.sample()
+            }).disposed(by: disposeBag)
     }
 }
--- Tone/SampleSkinToneView/SampleSkinToneViewModel.swift
+++ Tone/SampleSkinToneView/SampleSkinToneViewModel.swift
@@ -14,9 +14,36 @@ class SampleSkinToneViewModel {
         case cancel
     }
     
-    let events = PublishSubject<Event>()
+    enum UserFaceStates {
+        case ok
+        case noFaceFound
+        case tooDark
+        case tooBright
+        case faceGradient
+        case faceTooFar
+        
+        var message: String {
+            switch self {
+            case .ok: return "Looking Good!"
+            case .noFaceFound: return "Looking For You..."
+            case .tooDark: return "It's A Little Too Dark Here... Lets try again in a room with a bit more light"
+            case .tooBright: return "It's A Little Too Bright Here... Try facing away from the brightest light in the room or moving to a darker area"
+            case .faceTooFar: return "You're Too Far Away! Bring me closer to your face!"
+            case .faceGradient: return "You're Too Unevenly Lit! Try and face away from the brightest light in the room"
+            }
+        }
+    }
     
+    //let userPrompt = Variable<String?>(nil)
+    let userFaceState = BehaviorSubject<UserFaceStates>(value: .noFaceFound)
+    
+    let events = PublishSubject<Event>()
+        
     func cancel() {
         events.onNext(.cancel)
+    }
+    
+    func sample() {
+        print("Gettin that sample")
     }
 }
